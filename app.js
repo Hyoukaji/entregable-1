b9902d4b32c44e7def42e3a6eabf94bf3669eed0
Hyoukaji <florespedromartin@gmail.com>
2021-12-06 23:29:43 -0300

    Formularios

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ app.listen(3000,()=>
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
 });
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+});
+
+app.post("/procesar",(req,res)=>{
+    res.send("envio informacion")
+})
