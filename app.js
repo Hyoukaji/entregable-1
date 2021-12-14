3a4c8b1518b7fe860c1997dd278b82ea776a419e
Hyoukaji <florespedromartin@gmail.com>
2021-12-13 21:47:00 -0300

    process.env

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 app.use(express.static("public"));
 
-app.listen(3000,()=>
+app.listen(process.env.PORT || 3000,()=>
     console.log("Levantando un servidor con Express en 3000")
     )
 
